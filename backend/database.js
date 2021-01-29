fafdb13b92a73a5bf5cc42d433c541541b8dcb27
Alejandro Penalba <atapenalba@gmail.com>
2021-01-29 15:45:22 -0300

    modificando mongo Atlas

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://ata1609:2203casa@cluster0.b9epm.mongodb.net/centroMedico?retryWrites=true&w=majority";
+const connection = "mongodb+srv://ata1609:2203casa@prueba1.b9epm.mongodb.net/User?retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
+
+    
